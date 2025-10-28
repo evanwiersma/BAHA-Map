9fb5c8c46d0097c89435837498f142d33ed7ee0a
Evan Wiersma <evanwiersma@berkeley.edu>
2025-10-27 17:18:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhbndpZXJzbWEiLCJhIjoiY21oOXMzMWZxMTdiMTJrcTNjMGRyb2NtbSJ9.KVDmztD9nwgkx6cyE0_pHg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/evanwiersma/cmh9s81ce00qr01sre2df9sih',
+  center: [-122.27, 37.8],
+  zoom: 9
+    });
